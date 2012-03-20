e29fe7f849e136b349e8d629e9d5b0235f12ea6a
Zack Michener <zack@zackmichener.com>
2012-03-20 18:28:15 -0400

    moved credentials.js and installed socket.io

--- app.js
+++ app.js
@@ -4,7 +4,19 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , io = require('socket.io')
+  , redis = require('redis')
+  , twitter = require('ntwitter')
+  , credentials = require('./credentials.js');
+
+var client = redis.createClient();
+var t = new twitter({
+	consumer_key: credentials.consumer_key,
+	consumer_secret: credentials.consumer_secret,
+	access_token_key: credentials.access_token_key,
+	access_token_secret: credentials.access_token_secret
+});
 
 var app = module.exports = express.createServer();
 
@@ -33,4 +45,10 @@ app.get('/', routes.index);
 app.get('/query/:query', routes.query);
 
 app.listen(3000);
+
+var sio = io.listen(app);
+sio.sockets.on('connection', function (socket) {
+	console.log('A socket connected!');
+});
+
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,35 @@ exports.index = function(req, res){
 };
 
 exports.query = function(req, res) {
-	
-	// var redis = require('redis');
-	// var client = redis.createClient();
-	// res.render('query', { title: req.params.query});
+
+
+	t.verifyCredentials(function (error, data) {
+		if (error) {
+			console.log(error + '');
+			process.exit(1);
+		}
+	}).stream(
+		'statuses/filter',
+		{ track: [req.params.query] },
+		function(stream) {
+			stream.on('data', function(tweet) {
+				
+				// console.log(tweet.text);
+				// //if awesome is in the tweet text, increment the counter                                                                                                                                                                        
+				// if(tweet.text.match(/awesome/))
+				//     client.incr('awesome');
+				// if (tweet.text.match(/cool/))
+				//     client.incr('cool');
+				// if (tweet.text.match(/rad/))
+				//     client.incr('rad');
+				// if (tweet.text.match(/gnarly/))
+				//     client.incr('gnarly');
+				// if (tweet.text.match(/groovy/))
+				//     client.incr('groovy');
+			});
+		}
+	);
+
+	res.send(req.params.query);
 };
+
